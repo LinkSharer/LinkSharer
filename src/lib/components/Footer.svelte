ab16fa3b8ad326bc57d7983f2a5865e0b0c4b522
Sebastiano <sebastiano@racca.me>
2024-09-20 16:18:26 +0200

    style: change footer link color

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,18 +6,22 @@
 	<footer>
 		<p>
 			This website was made with
-			<a href="https://github.com/LinkSharer/LinkSharer" rel="noopener noreferrer external"
-				>LinkSharer</a
-			>
+			<a href="https://github.com/LinkSharer/LinkSharer" rel="noopener noreferrer external">
+				LinkSharer
+			</a>
 		</p>
 	</footer>
 {/if}
 
-<style>
+<style lang="scss">
 	p {
 		color: var(--credits-text-color);
 		font-size: var(--credits-font-size);
 		text-align: center;
 		margin: 10px;
+
+		a {
+			color: var(--credits-text-color);
+		}
 	}
 </style>

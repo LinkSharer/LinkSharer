83348deef6bef37695f3ff235672d3e3149e8bc0
Sebastiano <sebastiano@racca.me>
2024-12-05 20:24:08 +0100

    fix(links): add support for targets

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -14,7 +14,7 @@
 	const goTo = (e: Event) => {
 		if (link.short) {
 			e.preventDefault();
-			goto(`s/${link.short}`);
+			link.target !== 'self' ? window.open(`s/${link.short}`) : goto(`s/${link.short}`);
 		}
 	};
 </script>

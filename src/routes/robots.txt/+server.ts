60960922b3d6734808b1fc7a3dec6abcbe34d9ea
Sebastiano <sebastiano@racca.me>
2024-12-05 20:03:09 +0100

    fix(build): move `robots.txt` and `sitemap.xml` to `static/`

--- src/routes/robots.txt/+server.ts
+++
@@ -1,8 +0,0 @@
-import config from '../../../user/config';
-
-export const GET = ({ url }) => {
-	return new Response(`User-agent: *
-Allow: /
-Sitemap: https://${config.cname || url.hostname}/sitemap.xml
-`);
-};
